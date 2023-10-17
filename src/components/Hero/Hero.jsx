69cbd56ce8e3fc144a5ad70227de14294b86f3fa
AleksaYO <yura.aleksa.98@gmail.com>
2023-10-18 02:23:58 +0300

    add: hero & about

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,13 @@
 import css from "./Hero.module.css";
 
 const Hero = () => {
-  return <section className={css.hero}></section>;
+  return (
+    <section className={css.hero}>
+      <div className={css["title-box"]}>
+        <h1 className={css.title}>Hotel & restaurant “Kazka”</h1>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
