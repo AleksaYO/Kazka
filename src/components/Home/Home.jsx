520cea10fe275e0be24d13d487f1b77c2621d8d6
AleksaYO <yura.aleksa.98@gmail.com>
2023-10-18 18:07:04 +0300

    add: rooms

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import Hero from "../Hero/Hero";
+import Rooms from "../Rooms/Rooms";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Rooms />
     </>
   );
 };
