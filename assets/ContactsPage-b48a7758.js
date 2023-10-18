75e014ec52388ed08947e92d3808dfe03504e36e
AleksaYO <AleksaYO@users.noreply.github.com>
2023-10-18 18:07:46 +0000

    deploy: 9de75c40fb5955a291612b4f7f7ed326fd282312

---
+++ assets/ContactsPage-b48a7758.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-8ec8057a.js";const r=()=>t.jsx(t.Fragment,{}),n=()=>t.jsx(t.Fragment,{children:t.jsx(r,{})});export{n as default};
---
+++ assets/MenuPage-4a48dc6a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-8ec8057a.js";const r=()=>e.jsx(e.Fragment,{}),n=()=>e.jsx(e.Fragment,{children:e.jsx(r,{})});export{n as default};
