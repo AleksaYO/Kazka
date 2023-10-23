8aa48977c951c3a3dc395e2926c920ef48b762d6
AleksaYO <yura.aleksa.98@gmail.com>
2023-10-23 16:11:02 +0300

    add: modal

--- src/components/Rooms/Katalog.jsx
+++ src/components/Rooms/Katalog.jsx
@@ -1,7 +1,24 @@
 import lux1 from "../../images/rooms/lux/lux.jpeg";
 import lux2 from "../../images/rooms/lux/lux2.jpeg";
 import lux3 from "../../images/rooms/lux/lux3.jpeg";
+const lux = [
+  { url: lux1, key: 1 },
+  { url: lux2, key: 2 },
+  { url: lux3, key: 3 },
+];
 import standart1 from "../../images/rooms/standart/standart1.jpeg";
 import standart2 from "../../images/rooms/standart/standart2.jpeg";
+const standart = [
+  { url: standart1, key: 1 },
+  { url: standart2, key: 2 },
+];
 
-export { lux1, lux2, lux3, standart1, standart2 };
+import standartPlus1 from "../../images/rooms/sandart_plus/IMG_5163.jpeg";
+import standartPlus2 from "../../images/rooms/sandart_plus/IMG_5164.jpeg";
+import standartPlus3 from "../../images/rooms/sandart_plus/standart+.jpeg";
+const standartPlus = [
+  { url: standartPlus3, key: 1 },
+  { url: standartPlus2, key: 2 },
+  { url: standartPlus1, key: 3 },
+];
+export { lux, standart, standartPlus };
--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,73 +1,92 @@
 import css from "./Rooms.module.css";
-import "atropos/css";
-import Atropos from "atropos/react";
+import { useState } from "react";
+import { lux, standart, standartPlus } from "./Katalog";
 
 const Rooms = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [images, setImages] = useState([]);
+
+  const onOpenModal = (e) => {
+    setIsOpen(true);
+    if (e.currentTarget.id === "standart") {
+      setImages(standart);
+    } else if (e.currentTarget.id === "lux") {
+      setImages(lux);
+    } else {
+      setImages(standartPlus);
+    }
+    document.body.style.overflow = "hidden";
+  };
+
+  const onCloseModal = (e) => {
+    if (e.target === e.currentTarget) {
+      setIsOpen(false);
+      document.body.style.overflow = "unset";
+    }
+  };
+
   return (
-    <section className={css.rooms}>
-      <ul className={css["rooms_list"]}>
-        <li onClick={() => console.log(1)} className={css["rooms_item"]}>
-          <div id="app">
-            <Atropos className="my-atropos">
-              <div className={css.standart}>
-                <div data-atropos-offset="10" className={css["info_box"]}>
-                  <h3 data-atropos-offset="15" className={css["rooms_title"]}>
-                    Стандарт
-                  </h3>
-                  <p data-atropos-offset="15" className={css["rooms_price"]}>
-                    900 грн
-                  </p>
-                </div>
-              </div>
-            </Atropos>
-          </div>
-        </li>
-        <li className={css["rooms_item"]}>
-          <div id="app">
-            <Atropos className="my-atropos">
-              <div className={css["standart_plus"]}>
-                <div data-atropos-offset="10" className={css["info_box"]}>
-                  <h3 data-atropos-offset="15" className={css["rooms_title"]}>
-                    Стандарт +
-                  </h3>
-                  <p data-atropos-offset="15" className={css["rooms_price"]}>
-                    1200 грн
-                  </p>
-                </div>
-              </div>
-            </Atropos>
-          </div>
-        </li>
-        <li className={css["rooms_item"]}>
-          <div id="app">
-            <Atropos className="my-atropos">
-              <div className={css.lux}>
-                <div data-atropos-offset="10" className={css["info_box"]}>
-                  <h3 data-atropos-offset="15" className={css["rooms_title"]}>
-                    Напівлюкс
-                  </h3>
-                  <p data-atropos-offset="15" className={css["rooms_price"]}>
-                    1600 грн
-                  </p>
-                </div>
-              </div>
-            </Atropos>
-          </div>
-        </li>
-      </ul>
-      {/* <ul className={css["rooms_list"]}>
-        <li className={css["rooms_item"]}>
-          <Atropos className={css.atropos} shadowScale={1}>
-            <div className={css["text_bg"]}>
-              <h3 className={css["rooms_title"]}>Стандарт</h3>
-              <p className={css["rooms_price"]}>900 грн</p>
+    <>
+      <section className={css.rooms}>
+        {isOpen && (
+          <div onClick={onCloseModal} className={css.backdrop}>
+            <div className={css.modal}>
+              <ul className={css["modal-list"]}>
+                {images.map(({ url, key }) => {
+                  return (
+                    <li className={css["modal-item"]} key={key}>
+                      <img className={css.image} src={url} alt="img" />
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-          </Atropos>
-        </li>
-        <li className={css["rooms_item"]}></li>
-        <li className={css["rooms_item"]}></li>
-      </ul> */}
-    </section>
+          </div>
+        )}
+        <ul className={css["rooms_list"]}>
+          <li id="standart" onClick={onOpenModal} className={css["rooms_item"]}>
+            <div className={css.standart}>
+              <div className={css["info_box"]}>
+                <h3 className={css["rooms_title"]}>Стандарт</h3>
+                <p className={css["rooms_price"]}>900 грн</p>
+              </div>
+            </div>
+          </li>
+          <li
+            id="standart+"
+            onClick={onOpenModal}
+            className={css["rooms_item"]}
+          >
+            <div className={css["standart_plus"]}>
+              <div className={css["info_box"]}>
+                <h3 className={css["rooms_title"]}>Стандарт +</h3>
+                <p className={css["rooms_price"]}>1200 грн</p>
+              </div>
+            </div>
+          </li>
+          <li id="lux" onClick={onOpenModal} className={css["rooms_item"]}>
+            <div className={css.lux}>
+              <div className={css["info_box"]}>
+                <h3 className={css["rooms_title"]}>Напівлюкс</h3>
+                <p className={css["rooms_price"]}>1600 грн</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+        {/* <ul className={css["rooms_list"]}>
+          <li className={css["rooms_item"]}>
+            <Atropos className={css.atropos} shadowScale={1}>
+              <div className={css["text_bg"]}>
+                <h3 className={css["rooms_title"]}>Стандарт</h3>
+                <p className={css["rooms_price"]}>900 грн</p>
+              </div>
+            </Atropos>
+          </li>
+          <li className={css["rooms_item"]}></li>
+          <li className={css["rooms_item"]}></li>
+        </ul> */}
+      </section>
+    </>
   );
 };
 
