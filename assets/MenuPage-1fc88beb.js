8246379ec3e70f3662b9be7156b781482093110b
AleksaYO <AleksaYO@users.noreply.github.com>
2023-11-22 12:33:55 +0000

    deploy: d540970731498b038396f0a1af7c84d7c480291b

---
+++ assets/MenuPage-1fc88beb.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-98f19241.js";const r=()=>e.jsx(e.Fragment,{}),n=()=>e.jsx(e.Fragment,{children:e.jsx(r,{})});export{n as default};
--- assets/MenuPage-e553d78e.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-03459e4c.js";const r=()=>e.jsx(e.Fragment,{}),n=()=>e.jsx(e.Fragment,{children:e.jsx(r,{})});export{n as default};
